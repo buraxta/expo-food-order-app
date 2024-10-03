5eb0c1cf2ae4d07f8458911e1dd1d11f871cead4
buraxta <burakgulluler@gmail.com>
2024-10-03 22:39:43 +0300

    expo router screens

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,38 +1,5 @@
-import { Image, StyleSheet } from "react-native";
-import { Text, View } from "@/src/components/Themed";
-import Colors from "@/src/constants/Colors";
-import products from "@/assets/myData/products";
-const product = products[0];
+import { Redirect } from "expo-router";
 
-export default function TabOneScreen() {
-  return (
-    <View style={styles.container}>
-      <Image source={{ uri: product.image }} style={styles.image} />
-      <Text style={styles.title}>{product.name}</Text>
-      <Text style={styles.price}>${product.price}</Text>
-    </View>
-  );
+export default function TabIndex() {
+  return <Redirect href={"/menu/"} />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    backgroundColor: "white",
-    padding: 10,
-    borderRadius: 10,
-  },
-  image: {
-    width: "100%",
-    aspectRatio: 1,
-  },
-  title: {
-    fontSize: 18,
-    fontWeight: "bold",
-    color: Colors.light.text,
-  },
-  price: {
-    // fontSize: 20,
-    fontWeight: "bold",
-    // marginVertical: 10,
-    color: Colors.light.tint,
-  },
-});
---
+++ src/components/ProductListItem.tsx
@@ -0,0 +1,49 @@
+import { StyleSheet, Text, View, Image, Pressable } from "react-native";
+import Colors from "../constants/Colors";
+import { Product } from "../types";
+import { Link } from "expo-router";
+
+type ProductListItemProps = {
+  product: Product;
+};
+
+export const defaultPizzaImage =
+  "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/default.png";
+
+const ProductListItem = ({ product }: ProductListItemProps) => {
+  return (
+    <Link href={`/menu/${product.id}`} asChild>
+      <Pressable style={styles.container}>
+        <Image
+          source={{ uri: product.image || defaultPizzaImage }}
+          style={styles.image}
+        />
+        <Text style={styles.title}>{product.name}</Text>
+        <Text style={styles.price}>${product.price}</Text>
+      </Pressable>
+    </Link>
+  );
+};
+export default ProductListItem;
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "white",
+    padding: 10,
+    borderRadius: 20,
+    flex: 1,
+  },
+  image: {
+    width: "100%",
+    aspectRatio: 1,
+    resizeMode: "contain",
+  },
+  title: {
+    fontSize: 18,
+    fontWeight: "600",
+    marginVertical: 10,
+  },
+  price: {
+    color: Colors.light.tint,
+    fontWeight: "bold",
+  },
+});
