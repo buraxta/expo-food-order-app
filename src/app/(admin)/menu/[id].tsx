1cc7181ef0155a60ebac4cc3d6224cc8e2f9a9ba
buraxta <burakgulluler@gmail.com>
2024-10-05 13:14:17 +0300

    admin side

---
+++ src/app/(admin)/menu/[id].tsx
@@ -0,0 +1,82 @@
+import { View, Text, Image, StyleSheet, Pressable } from "react-native";
+import React, { useState } from "react";
+import { Stack, useLocalSearchParams, useRouter } from "expo-router";
+import products from "@/assets/myData/products";
+import Colors from "@/src/constants/Colors";
+import { defaultPizzaImage } from "@/src/components/ProductListItem";
+import Button from "@/src/components/Button";
+import { useCart } from "@/src/providers/CartProvider";
+import { PizzaSize } from "@/src/types";
+
+const ProductDetailsScreen = () => {
+  const { id } = useLocalSearchParams();
+  const product = products.find((p) => p.id === Number(id));
+  const { addItem } = useCart();
+  const [selectedSize, setSetselectedSize] = useState<PizzaSize>("M");
+  const router = useRouter();
+
+  const addToCard = () => {
+    if (!product) return;
+    addItem(product, selectedSize);
+    router.push("/cart");
+  };
+
+  if (!product) {
+    return <Text>Product not found</Text>;
+  }
+
+  return (
+    <View style={styles.container}>
+      <Stack.Screen options={{ title: product.name }} />
+      <View>
+        <Image
+          source={{ uri: product.image || defaultPizzaImage }}
+          style={styles.image}
+        />
+      </View>
+
+      <Text style={styles.priceText}>Title: {product?.name}</Text>
+      <Text style={styles.priceText}>Price: ${product?.price}</Text>
+    </View>
+  );
+};
+
+export default ProductDetailsScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "white",
+    padding: 10,
+    borderRadius: 20,
+    flex: 1,
+  },
+  image: {
+    width: "100%",
+    aspectRatio: 1,
+    resizeMode: "contain",
+  },
+  sizeContainer: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    marginVertical: 10,
+  },
+  sizeButton: {
+    width: 50,
+    aspectRatio: 1,
+    borderRadius: 25,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  priceText: {
+    fontSize: 18,
+    fontWeight: "600",
+    marginVertical: 10,
+  },
+  addToCartButton: {
+    backgroundColor: Colors.light.tint,
+    padding: 15,
+    borderRadius: 50,
+    alignItems: "center",
+    color: "white",
+  },
+});
---
+++ src/app/(admin)/menu/index.tsx
@@ -0,0 +1,25 @@
+import { FlatList, View } from "react-native";
+import products from "@assets/myData/products";
+import ProductListItem from "@components/ProductListItem";
+import { Stack } from "expo-router";
+
+export default function MenuScreen() {
+  return (
+    <View>
+      <FlatList
+        data={products}
+        renderItem={({ item }) => {
+          return <ProductListItem product={item} />;
+        }}
+        numColumns={2}
+        contentContainerStyle={{
+          padding: 10,
+          gap: 10,
+        }}
+        columnWrapperStyle={{
+          gap: 10,
+        }}
+      />
+    </View>
+  );
+}
