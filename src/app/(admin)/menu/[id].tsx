9caa9f5fa3a57b48d6f3ee29f222078bcade617b
buraxta <burakgulluler@gmail.com>
2024-10-06 11:40:52 +0300

    update and delete ui

--- src/app/(admin)/menu/[id].tsx
+++ src/app/(admin)/menu/[id].tsx
@@ -1,12 +1,13 @@
 import { View, Text, Image, StyleSheet, Pressable } from "react-native";
 import React, { useState } from "react";
-import { Stack, useLocalSearchParams, useRouter } from "expo-router";
+import { Link, Stack, useLocalSearchParams, useRouter } from "expo-router";
 import products from "@/assets/myData/products";
 import Colors from "@/src/constants/Colors";
 import { defaultPizzaImage } from "@/src/components/ProductListItem";
 import Button from "@/src/components/Button";
 import { useCart } from "@/src/providers/CartProvider";
 import { PizzaSize } from "@/src/types";
+import { FontAwesome } from "@expo/vector-icons";
 
 const ProductDetailsScreen = () => {
   const { id } = useLocalSearchParams();
@@ -27,7 +28,25 @@ const ProductDetailsScreen = () => {
 
   return (
     <View style={styles.container}>
-      <Stack.Screen options={{ title: product.name }} />
+      <Stack.Screen
+        options={{
+          title: "Menu",
+          headerRight: () => (
+            <Link href={`/(admin)/menu/create/?id=${id}`} asChild>
+              <Pressable>
+                {({ pressed }) => (
+                  <FontAwesome
+                    name="pencil"
+                    size={25}
+                    color={Colors.light.tint}
+                    style={{ marginRight: 15, opacity: pressed ? 0.5 : 1 }}
+                  />
+                )}
+              </Pressable>
+            </Link>
+          ),
+        }}
+      />
       <View>
         <Image
           source={{ uri: product.image || defaultPizzaImage }}
