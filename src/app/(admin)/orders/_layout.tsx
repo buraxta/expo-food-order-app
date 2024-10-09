dba3e2a2c61901d3718824f849bded6c024a878d
buraxta <burakgulluler@gmail.com>
2024-10-09 18:49:17 +0300

    add top tap navigator

--- src/app/(admin)/orders/_layout.tsx
+++ src/app/(admin)/orders/_layout.tsx
@@ -1,9 +1,13 @@
-import { View, Text } from "react-native";
-import React from "react";
 import { Stack } from "expo-router";
+import React from "react";
 
 const _layout = () => {
-  return <Stack />;
+  return (
+    <Stack>
+      {/* <Stack.Screen name="index" options={{ title: "Orders" }} /> */}
+      <Stack.Screen name="list" options={{ headerShown: false }} />
+    </Stack>
+  );
 };
 
 export default _layout;
